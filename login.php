83a14b00b13a13df42ac74346fff62643b9d2359
peter_pan08 <info@dryba.com>
2025-05-22 20:56:44 +0200

    Strukturänderung

--- login.php
+++ login.php
@@ -28,6 +28,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html lang="de">
 <head>
+    <div class="top-links">
+    <a href="admin.html">🔧 Adminbereich</a>
+    <a href="admin_user.php">👥 Benutzerverwaltung</a>
+    <a href="index.html">⬅️ Zurück zur Startseite</a>
+  </div>
   <meta charset="UTF-8">
   <title>Login</title>
   <style>

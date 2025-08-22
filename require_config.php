00a4541a013b9908e32a7ebea4687052c9a59b93
peter_pan08 <peter-pan08@users.noreply.github.com>
2025-08-22 21:00:28 +0200

    Add global DEBUG switch and centralize error handling

--- require_config.php
+++ require_config.php
@@ -5,4 +5,14 @@ if (!file_exists($cfg)) {
     exit;
 }
 require_once $cfg;
+
+if (defined('DEBUG') && DEBUG) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+    error_reporting(0);
+}
 ?>
